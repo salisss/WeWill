4f232e97886084e5e6b87fc41228cbfd4bb1d860
sasa <sasa@sasa-PC>
2016-07-01 17:23:09 +0800

    login

--- WeWill/src/com/sasa/wewill/ui/ProductListActivity.java
+++ WeWill/src/com/sasa/wewill/ui/ProductListActivity.java
@@ -1,8 +1,18 @@
 package com.sasa.wewill.ui;
 
+import com.sasa.wewill.R;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
 
 public class ProductListActivity extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);  //去除标题栏
+		setContentView(R.layout.productlist);
+	}
 	
 }
